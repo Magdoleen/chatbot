27df2290471d7e93c6978a30e1594dcbda844bb9
unknown <areeg.ahmed@hotmail.com>
2020-12-09 18:06:15 +0200

    en model

--- chatbot.py
+++ chatbot.py
@@ -29,4 +29,5 @@ def get_bot_response():
     return str(bot.get_response(userText))
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
